e322d505e2d975658c1a1cbcacab61f1faff85bf
Austin <aland@epsilonsystems.com>
2026-07-08 00:28:32 -0400

    Add fundamental ZarrChunk class

---
+++ cell_tracking/types_.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import numpy as np
+from stonesoup.base import Property
+from stonesoup.types.sensordata import SensorData
+
+class ZarrChunk(SensorData):
+    """ Zarr Chunk type used to represent a single volume chunk from a Zarr directory """
+
+    voxels: np.ndarray = Property(
+        doc="An array of shape (Z,Y,X) containing the individual voxel values, where Z,Y,"
+        "X are the three physical dimensions of the 3D volume.")
+    scale: np.ndarray = Property(
+        doc="An array of length three, ordered z,y,x. This is the physical scale" \
+        "of the voxel in each of the three dimensions."
+    )
+    timestamp: datetime = Property(doc="An optional timestamp", default=None)
+
+    def __bool__(self):
+        return len(self.voxels) > 0
